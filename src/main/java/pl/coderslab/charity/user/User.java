0f0dc5cd160c3cf744019f45000ac41e0d1928f4
Kinga-Java <kinga.gozdziuk@gmail.com>
2020-10-25 18:17:01 +0100

    user entity

---
+++ src/main/java/pl/coderslab/charity/user/User.java
@@ -0,0 +1,53 @@
+package pl.coderslab.charity.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = User.TABLE)
+public class User {
+    public final static String TABLE = "user";
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @NotBlank
+    @Column(nullable = false)
+    @Size(min = 8)
+    private String password;
+
+    @NotBlank
+    @Transient
+    private String repeatPassword;
+
+    @NotBlank
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private Boolean active;
+
+    @ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+}
