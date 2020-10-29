5d73d78626181755b73b490d1ff5f97a8c91cb7a
honestit <michal.kupisinski@gmail.com>
2020-10-29 20:09:58 +0100

    Resty esty testy

--- src/main/java/pl/coderslab/charity/institution/Institution.java
+++ src/main/java/pl/coderslab/charity/institution/Institution.java
@@ -29,5 +29,4 @@ public class Institution {
     @Column(nullable = false)
     private String description;
 
-
 }
---
+++ src/main/java/pl/coderslab/charity/institution/InstitutionRestController.java
@@ -0,0 +1,76 @@
+package pl.coderslab.charity.institution;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/institutions")
+public class InstitutionRestController {
+
+    // Kodów odpowiedz i nagłówków
+
+    private final InstitutionService institutionService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Institution> getAllInstitutions() {
+        return institutionService.getAll();
+    }
+
+    @PostMapping
+    // 201 Created - poprawny zapis wartości i taki status zwracamy
+    //               + Location: adres utworzonego zasobu, np: /api/institutions/14
+    // 400 Bad Request
+    public ResponseEntity saveInstitution(@Valid @RequestBody Institution institution) {
+        institutionService.addInstitution(institution);
+        return ResponseEntity.created(URI.create("/api/institutions/" + institution.getId())).build();
+//        return ResponseEntity.status(HttpStatus.CREATED).location(URI.create("/api/institutions/"+ institution.getId())).build();
+    }
+
+    @GetMapping("/{id}")
+    // 200 OK - jak znaleziono
+    // 404 Not found - jak nie znaleziono
+    public ResponseEntity getOne(@PathVariable long id) {
+        Institution institution = institutionService.findInstitutionById(id);
+        if (institution != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(institution);
+//            return ResponseEntity.ok(institution);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void saveEditedInstitution(@PathVariable long id, @Valid @RequestBody Institution institution) {
+        if (institution.getId() != id) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bzdet");
+        }
+        institutionService.updateInstitution(institution);
+    }
+
+    @DeleteMapping("/{id}")
+    // 200 OK - jak usunięto, bez treści
+    // 404 Not found - czegoś czego nie ma
+    public ResponseEntity deleteInstitutionById(@PathVariable long id){
+        Institution institution = institutionService.findInstitutionById(id);
+        if (institution != null) {
+            institutionService.deleteInstitutionById(id);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
